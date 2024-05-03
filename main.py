b3b06346007ae0296a39bf6b0868748ae2a9d352
ErenH04 <bs@redrockcom.com>
2024-05-02 22:03:16 -0400

    Code is finished

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(f"{count_words} words found in the document.")
+    num_words = count_words(text) 
+    print(f"{num_words} words found in the document.")
 
 def get_book_text(path):
     with open(path) as f:
