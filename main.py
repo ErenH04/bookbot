8e58cb9af16e68db04a77d709a4efbddf29d2ebf
ErenH04 <bs@redrockcom.com>
2024-05-02 21:00:53 -0400

    edited the functions and added a print statement for testing.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    print(f"{count_words} words found in the document.")
 
 def get_book_text(path):
     with open(path) as f:
-        return f.read()
+        return f.read() 
 
-main() 
+def count_words(text):
+    words = text.split()
+    return len(words)
 
-
+main()
